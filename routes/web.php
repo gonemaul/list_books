1428c77f36da2ae16b283445da351f941723d2c3
Maulana <wongdesoo366@gmail.com>
2024-07-26 11:01:58 +0700

    coba cicd

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/books', 301);
 Route::resource('books', BookController::class);
+Route::get('test', function(){
+    return 'oke';
+});
